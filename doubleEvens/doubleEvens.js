1350b0c93398ee6b22c214f0d5cacd036c6add04
jv <jv@jvworldwide.com>
2018-07-26 18:45:38 -0500

    added solution for Double Evens

---
+++ doubleEvens/doubleEvens.js
@@ -0,0 +1,10 @@
+const doubleEvens = function(arrayToDouble) {
+    let inputArray = arrayToDouble;
+    let filtered = inputArray.filter(a => inputArray.indexOf(a)%2===1);
+    let multiply = filtered.map(x => x * 2);
+  
+    console.log(filtered);
+    console.log(multiply);
+  }
+  
+  doubleEvens([1,2,3,4,5,6]);
