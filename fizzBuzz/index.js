0829a82a2018defda95291afe0bd28b6d4719002
jv <jv@jvworldwide.com>
2018-07-27 11:26:59 -0500

    solution using 3 && 5 && 15 with the % oppr

--- fizzBuzz/index.js
+++ fizzBuzz/index.js
@@ -12,6 +12,13 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+const fizzBuzz = function(num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 15 === 0) console.log('FizzBUZZ');
+    else if (i % 3 === 0) console.log('Fizz');
+    else if (i % 5 === 0) console.log('BUZZ');
+    else console.log(i);
+  }    
+}
 
 module.exports = fizzBuzz;
