9fa85f355d889c448ad24432ceac56007e3a085f
jv <jv@jvworldwide.com>
2018-07-26 19:27:27 -0500

    updated instructions

--- doubleEvens/doubleEvens.js
+++ doubleEvens/doubleEvens.js
@@ -2,9 +2,10 @@ const doubleEvens = function(arrayToDouble) {
     let inputArray = arrayToDouble;
     let filtered = inputArray.filter(a => inputArray.indexOf(a)%2===1);
     let multiply = filtered.map(x => x * 2);
-  
-    console.log(filtered);
-    console.log(multiply);
-  }
-  
-  doubleEvens([1,2,3,4,5,6]);
+
+//    console.log(filtered);
+//    console.log(multiply);
+}
+ // doubleEvens([1,2,3,4,5,6]);
+ 
+ module.exports = doubleEvens;
